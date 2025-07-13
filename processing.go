8714146a82b794e2a83f7a0f47660fda0d17e916
Jared Talbot <jared.talbot-2@selu.edu>
2025-07-12 22:49:06 -0500

    feat: change image resize behavior

--- processing.go
+++ processing.go
@@ -48,8 +48,6 @@ func GetNRGBA(c color.Color) color.NRGBA {
 // 	return absDiff(int(a.X), int(b.X)) + absDiff(int(a.Y), int(b.Y))
 // }
 
-var ErrImageTooWide = errors.New("image is too wide")
-
 type RegionPixel byte
 
 const (
@@ -280,24 +278,37 @@ func MatrixToImage(matrix [][]bool) *image.Paletted {
 	return result
 }
 
-func ResizeImage(img image.Image) (image.Image, error) {
+// Resizes the image to the default 1920x1080. Uses [ResizeImageTo].
+func ResizeImage(img image.Image) image.Image {
 	const MAX_HEIGHT = 1080
-	const MAX_WIDTH = 2000
+	const MAX_WIDTH = 1920
 
+	return ResizeImageTo(img, MAX_WIDTH, MAX_HEIGHT)
+}
+
+// Constrains the image to the given dimensions, preserving aspect ratio.
+// If either dimension is set to 0 or less, it will be ignored (effectively like if you set it to infinity).
+func ResizeImageTo(img image.Image, width, height int) image.Image {
 	bd := img.Bounds()
-	if bd.Dy() > MAX_HEIGHT {
-		scalar := float64(MAX_HEIGHT) / float64(bd.Dy())
-		newWidth := math.Round(float64(bd.Dx()) * scalar)
-		if newWidth > MAX_WIDTH {
-			return nil, ErrImageTooWide
-		}
-		scaledImg := image.NewNRGBA(image.Rect(0, 0, int(newWidth), MAX_HEIGHT))
-		draw.NearestNeighbor.Scale(scaledImg, scaledImg.Rect, img, img.Bounds(), draw.Over, nil)
-		return scaledImg, nil
-	} else if bd.Dx() > MAX_WIDTH {
-		return nil, ErrImageTooWide
+	if (width <= 0 && height <= 0) || (width <= bd.Dx() && height <= bd.Dy()) {
+		width, height = bd.Dx(), bd.Dy()
+	} else if width <= 0 {
+		wScalar := float64(height) / float64(bd.Dy())
+		width = int(math.Round(float64(bd.Dx()) * wScalar))
+	} else if height <= 0 {
+		hScalar := float64(width) / float64(bd.Dx())
+		height = int(math.Round(float64(bd.Dx()) * hScalar))
+	} else {
+		wScalar := float64(height) / float64(bd.Dy())
+		hScalar := float64(width) / float64(bd.Dx())
+		scalar := math.Min(wScalar, hScalar)
+		width = int(math.Round(float64(bd.Dx()) * scalar))
+		height = int(math.Round(float64(bd.Dx()) * scalar))
 	}
-	return img, nil
+
+	scaledImg := image.NewNRGBA(image.Rect(0, 0, width, height))
+	draw.NearestNeighbor.Scale(scaledImg, scaledImg.Rect, img, img.Bounds(), draw.Over, nil)
+	return scaledImg
 }
 
 func SimplifyImage(img image.Image, options RegionMapOptions) (result image.Image) {
