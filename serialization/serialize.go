30c9b0c4ca9ab0183aa52bfcfdf173ebf2cf4811
Jared Talbot <jared.talbot-2@selu.edu>
2025-08-21 17:19:38 -0500

    feat: json serialization

--- serialization/serialize.go
+++ serialization/serialize.go
@@ -4,8 +4,11 @@ import (
 	main "boardshapes/boardshapes"
 	v0_1 "boardshapes/boardshapes/serialization/v0.1"
 	"bytes"
+	"encoding/base64"
 	"encoding/binary"
+	"encoding/json"
 	"errors"
+	"image"
 	"image/color"
 	"image/png"
 	"io"
@@ -194,4 +197,101 @@ func BinaryDeserialize(r io.Reader, options map[string]any) (*main.BoardshapesDa
 	}
 
 	return deserializeFunc(&buf, options)
+}
+
+type JSONData struct {
+	Version string          `json:"version"`
+	Shapes  []JSONShapeData `json:"shapes"`
+}
+
+type JSONShapeData struct {
+	Number      int         `json:"number"`
+	CornerX     int         `json:"corner_x"`
+	CornerY     int         `json:"corner_y"`
+	Shape       []uint16    `json:"path"`
+	Color       color.NRGBA `json:"color"`
+	ColorString string      `json:"color_string"`
+	Image       string      `json:"image"`
+}
+
+func JsonSerialize(data *main.BoardshapesData) ([]byte, error) {
+	jsonData := JSONData{
+		Version: data.Version,
+		Shapes:  make([]JSONShapeData, len(data.Shapes)),
+	}
+
+	for i, shape := range data.Shapes {
+		points := make([]uint16, len(shape.Path)*2)
+		for j, v := range shape.Path {
+			points[j*2] = v.X
+			points[j*2+1] = v.Y
+		}
+
+		var imgBase64 string
+		if shape.Image != nil {
+			buf := new(bytes.Buffer)
+			if err := png.Encode(buf, shape.Image); err != nil {
+				return nil, err
+			}
+			imgBase64 = base64.StdEncoding.EncodeToString(buf.Bytes())
+		}
+
+		jsonData.Shapes[i] = JSONShapeData{
+			Number:      shape.Number,
+			CornerX:     shape.CornerX,
+			CornerY:     shape.CornerY,
+			Shape:       points,
+			Color:       shape.Color.(color.NRGBA),
+			ColorString: shape.ColorName,
+			Image:       imgBase64,
+		}
+	}
+
+	return json.Marshal(jsonData)
+}
+
+func JsonDeserialize(d []byte) (*main.BoardshapesData, error) {
+	var jsonData JSONData
+	if err := json.Unmarshal(d, &jsonData); err != nil {
+		return nil, err
+	}
+
+	data := &main.BoardshapesData{
+		Version: jsonData.Version,
+		Shapes:  make([]main.ShapeData, len(jsonData.Shapes)),
+	}
+
+	for i, jsonShape := range jsonData.Shapes {
+		path := make([]main.Vertex, len(jsonShape.Shape)/2)
+		for j := range path {
+			path[j] = main.Vertex{
+				X: jsonShape.Shape[j*2],
+				Y: jsonShape.Shape[j*2+1],
+			}
+		}
+
+		var img image.Image
+		if jsonShape.Image != "" {
+			imgBytes, err := base64.StdEncoding.DecodeString(jsonShape.Image)
+			if err != nil {
+				return nil, err
+			}
+			img, err = png.Decode(bytes.NewReader(imgBytes))
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		data.Shapes[i] = main.ShapeData{
+			Number:    jsonShape.Number,
+			CornerX:   jsonShape.CornerX,
+			CornerY:   jsonShape.CornerY,
+			Path:      path,
+			Color:     jsonShape.Color,
+			ColorName: jsonShape.ColorString,
+			Image:     img,
+		}
+	}
+
+	return data, nil
 }
