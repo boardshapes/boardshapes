5f88da7ba49975415676870afedd4ef112112e28
Jared Talbot <jared.talbot-2@selu.edu>
2025-08-27 23:08:48 -0500

    fix: clean things up and better flags

--- cli_processing/cli_tool.go
+++ cli_processing/cli_tool.go
@@ -2,20 +2,53 @@ package main
 
 import (
 	"boardshapes/boardshapes"
+	"errors"
 	"flag"
 	"fmt"
 	"image"
 	"image/jpeg"
 	"image/png"
+	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
-var rFlag = flag.Bool("r", false, "rflag this should resize an image")
-var sFlag = flag.String("s", "", "sflag gets a user file output name.")
-var mFlag = flag.Bool("m", false, "mflag simplifies an image into regions.")
-var jFlag = flag.Bool("j", false, "jflag should meshify an image")
+var resizeImage string
+var outputSimplifiedImagePath string
+var noShapes bool
+var binaryOutput bool
+var outputPath string
+var useStdOut bool
+
+func init() {
+	const resizeFlagDescription = "Resize the image to fit a specific size while maintaining aspect ratio. " +
+		"Value should be in the format [width]x[height] where both width and height " +
+		"are optional and can be left empty. If neither are specified, it will default to 1920x1080."
+	flag.StringVar(&resizeImage, "r", "no", resizeFlagDescription)
+	flag.StringVar(&resizeImage, "resize", "no", resizeFlagDescription)
+
+	const outputSimplifiedFlagDescription = "If set to a valid filepath, the simplified image generated will " +
+		"be output to that filepath."
+	flag.StringVar(&outputSimplifiedImagePath, "s", "", outputSimplifiedFlagDescription)
+	flag.StringVar(&outputSimplifiedImagePath, "output-simplified", "", outputSimplifiedFlagDescription)
+
+	const noShapesFlagDescription = "Skips the shape generation step and does not output any shape data."
+	flag.BoolVar(&noShapes, "x", false, noShapesFlagDescription)
+	flag.BoolVar(&noShapes, "no-shapes", false, noShapesFlagDescription)
+
+	const binaryFlagDescription = "Serializes shape data to the binary format instead of JSON."
+	flag.BoolVar(&binaryOutput, "b", false, binaryFlagDescription)
+	flag.BoolVar(&binaryOutput, "binary", false, binaryFlagDescription)
+
+	const outputFileFlagDescription = "Path to the output file"
+	flag.StringVar(&outputPath, "o", "output.bshapes", outputFileFlagDescription)
+	flag.StringVar(&outputPath, "output", "output.bshapes", outputFileFlagDescription)
+
+	const useStdOutFlagDescription = "If set, the output will be written to stdout instead of a file."
+	flag.BoolVar(&useStdOut, "c", false, useStdOutFlagDescription)
+}
 
 func main() {
 
@@ -27,47 +60,45 @@ func main() {
 		panic(err)
 	}
 
-	if *rFlag {
-		img = boardshapes.ResizeImage(img)
-	}
-
-	if *jFlag {
-		simplifiedImage := boardshapes.SimplifyImage(img, boardshapes.RegionMapOptions{})
-
-		for i := 0; i < regionCount-1; i++ {
-			currRegion := regionToOutput.GetRegion(boardshapes.RegionIndex(i))
-			regionMeshCreated, err := currRegion.CreateMesh()
-
+	if resizeImage != "no" {
+		if resizeImage == "" {
+			img = boardshapes.ResizeImage(img)
+		}
+		dimensions := strings.Split(resizeImage, "x")
+		var width, height int
+		if len(dimensions) != 2 {
+			panic(errors.New("invalid resize format: Use [width]x[height], e.g. 800x600, 800x, x600"))
+		}
+		if dimensions[0] != "" {
+			var err error
+			width, err = strconv.Atoi(dimensions[0])
 			if err != nil {
-				panic(err)
+				panic(errors.New("invalid width value"))
 			}
-			for j := 0; j < len(regionMeshCreated); j++ {
-				fmt.Println("X: ", regionMeshCreated[j].X, "Y: ", regionMeshCreated[j].Y)
+		}
+		if dimensions[1] != "" {
+			var err error
+			height, err = strconv.Atoi(dimensions[1])
+			if err != nil {
+				panic(errors.New("invalid height value"))
 			}
+		}
+		if width == 0 && height == 0 {
+			img = boardshapes.ResizeImage(img)
+		} else {
+			img = boardshapes.ResizeImageTo(img, width, height)
 		}
 	}
 
-	if *mFlag { // im sorry ok.
-		fileRegioned, regionCount, _ := boardshapes.SimplifyImage(img, boardshapes.RegionMapOptions{})
-		fmt.Println(regionCount)
+	if outputSimplifiedImagePath != "" {
+		simplifiedImage := boardshapes.SimplifyImage(img, boardshapes.ShapeCreationOptions{})
 
-		outputFile := fileEncoder(fileRegioned)
-		image_output(outputFile, filepath.Dir(fileInput[0]))
-	} else {
-		outputFile := fileEncoder(img)
-		image_output(outputFile, filepath.Dir(fileInput[0]))
+		encodeImageToFile(simplifiedImage, outputSimplifiedImagePath)
 	}
-
-	fmt.Println("1:", *rFlag)
-	fmt.Println("2:", *sFlag)
-	fmt.Println("3:", *mFlag)
-	fmt.Println("3.5:", *jFlag)
-	fmt.Println("4:", fileInput)
 
 }
 
 func decodeImageFromFile(fileInput []string) (image.Image, error) {
-
 	joinedFileName := strings.Join(fileInput, "")
 
 	fileTaken, err := os.Open(joinedFileName)
@@ -91,19 +122,17 @@ func decodeImageFromFile(fileInput []string) (image.Image, error) {
 	return nil, fmt.Errorf("unsuppported file format")
 }
 
-func fileEncoder(img image.Image) *os.File {
-
-	var outputPath string
-	if *sFlag != "" {
-		outputPath = *sFlag
-	} else {
-		outputPath = "output.png"
-	}
-	outputFile, err := os.Create(outputPath)
+func encodeImageToFile(img image.Image, outPath string) *os.File {
+	err := os.MkdirAll(filepath.Dir(outPath), 0755)
 	if err != nil {
 		panic(err)
 	}
-	ext := strings.ToLower(filepath.Ext(outputPath))
+	outputFile, err := os.Create(outPath)
+	if err != nil {
+		panic(err)
+	}
+
+	ext := strings.ToLower(filepath.Ext(outPath))
 	switch ext {
 	case ".png":
 		err = png.Encode(outputFile, img)
@@ -113,17 +142,6 @@ func fileEncoder(img image.Image) *os.File {
 	if err != nil {
 		panic(err)
 	}
+	log.Printf("Output file path: %s\n", outPath)
 	return outputFile
-}
-
-func image_output(fileToOutput *os.File, inputDir string) {
-
-	outputDir := filepath.Join(inputDir, "output_files")
-	err := os.MkdirAll(outputDir, 0755)
-	if err != nil {
-		fmt.Printf("Error creating output directory: %v\n", err)
-		return
-	}
-	outputFilePath := filepath.Join(outputDir, filepath.Base(fileToOutput.Name()))
-	fmt.Printf("Output file path: %s\n", outputFilePath)
 }
