914a45fad5eec842e9c908647cec5e909f409b1b
Jared Talbot <jared.talbot-2@selu.edu>
2025-10-12 19:41:56 -0500

    update cli tool build script

--- cli_tool/build.sh
+++ cli_tool/build.sh
@@ -10,14 +10,20 @@ PLATFORMS=(
   "windows/arm64"
   "js/wasm"
 )
+VERSION=$1
+
+if [ -z "$VERSION" ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
 
 mkdir -p $OUTPUT_DIR
 
 for PLATFORM in "${PLATFORMS[@]}"
 do
   IFS="/" read -r GOOS GOARCH <<< "$PLATFORM"
-  OUTPUT_NAME="$APP_NAME-$GOOS-$GOARCH"
+  OUTPUT_NAME="$APP_NAME-$VERSION-$GOOS-$GOARCH"
   [ "$GOOS" = "windows" ] && OUTPUT_NAME+=".exe"
-  [ "$GOOS" = "js" ] && OUTPUT_NAME="$APP_NAME.wasm"
+  [ "$GOOS" = "js" ] && OUTPUT_NAME="$APP_NAME-$VERSION.wasm"
   env GOOS=$GOOS GOARCH=$GOARCH go build -o "$OUTPUT_DIR/$OUTPUT_NAME"
 done
